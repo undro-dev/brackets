ce8b017d02859cee596cf02cce906802e5e626ed
Vitaly Undro <vitalyundro@gmail.com>
2023-08-27 17:54:44 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let brackets = bracketsConfig.map(el => el.join('')).join('');
+
+	let stack = [];
+
+	for (let bracket of str) {
+		let bracketsIndex = brackets.indexOf(bracket);
+
+		let isSimilar = brackets[bracketsIndex] === brackets[bracketsIndex + 1];
+
+		if (isSimilar && stack[stack.length - 1] !== bracket) {
+			stack.push(bracket);
+		} else if (isSimilar && stack[stack.length - 1] === bracket) {
+			stack.pop();
+		} else if (bracketsIndex % 2 === 0 && !isSimilar) {
+			stack.push(bracketsIndex + 1);
+		} else {
+			if (stack.pop() !== bracketsIndex) {
+				return false;
+			}
+		}
+	}
+
+	return stack.length === 0;
+};
